690793eee1ec1439d8f7134cba86e6958f3411a4
Silver0006 <52266188+Silver0006@users.noreply.github.com>
2025-10-26 16:05:00 -0400

    Added delay

--- consumer.cpp
+++ consumer.cpp
@@ -7,14 +7,16 @@
 #include <iostream> 
 
 int main(){
+    sleep(2); // Makes sure consumer doesn't start before producer
     sem_t *sem_full = sem_open("/sem_full", 0); // Indicator if buffer is full
     sem_t *sem_empty = sem_open("/sem_empty", 0); // Indicator if buffer is empty
     sem_t *sem_mutex = sem_open("/sem_mutex", 0); // If process is modifying shared memory
+    int index = 0; // Iterator for array
     int shm_fd = shm_open("/shared_data", O_RDWR, 0666); // Create shared memory object
     int *cart = (int*) mmap(
         NULL, // Address starting point/hint thing?
         2*sizeof(int), // Allocation size
-        PROT_READ, // Permision to write to shared data
+        PROT_READ | PROT_WRITE, // Permision to read/write to shared data
         MAP_SHARED, // Permision to make viewable to other processes
         shm_fd, //Shared memory
         0 // Adress offset
@@ -23,9 +25,11 @@ int main(){
     while(true) {
         sem_wait(sem_full);
         sem_wait(sem_mutex); 
-        std::cout << "Consumer: " << cart[0] << ' ' << cart[1] << std::endl;
+        std::cout << "Consumer: " << cart[index] << std::endl;
+        index = (index + 1) % 2;
         sem_post(sem_mutex);
         sem_post(sem_empty);
+        sleep(1);
     }
     
 
--- producer.cpp
+++ producer.cpp
@@ -12,15 +12,15 @@ int main(){
     sem_unlink("/sem_empty");
     sem_unlink("/sem_mutex");
     sem_t *sem_full = sem_open("/sem_full", O_CREAT, 0666, 0); // Indicator if buffer is full
-    sem_t *sem_empty = sem_open("/sem_empty", O_CREAT, 0666, 1); // Indicator if buffer is empty
+    sem_t *sem_empty = sem_open("/sem_empty", O_CREAT, 0666, 2); // Indicator if buffer is empty
     sem_t *sem_mutex = sem_open("/sem_mutex", O_CREAT, 0666, 1); // If process is modifying shared memory
-
+    int index = 0; // Iterator for array
     int shm_fd = shm_open("/shared_data", O_CREAT | O_RDWR, 0666); // Create shared memory object
     ftruncate(shm_fd, 2*sizeof(int)); // Set shared memory to size of passable_data structure.
     int *cart = (int*) mmap(
         NULL, // Address starting point/hint thing?
         2*sizeof(int), // Allocation size
-        PROT_WRITE, // Permision to write to shared data
+        PROT_READ | PROT_WRITE, // Permision to read/write to shared data
         MAP_SHARED, // Permision to make viewable to other processes
         shm_fd, //Shared memory
         0 // Adress offset
@@ -31,13 +31,15 @@ int main(){
 
 
     while(true) {
+        int produce = std::rand();
         sem_wait(sem_empty);
         sem_wait(sem_mutex); 
-        cart[0] = std::rand();
-        cart[1] = std::rand();
-        std::cout << "Producer: " << cart[0] << ' ' << cart[1] << std::endl;
+        cart[index] = produce;
+        std::cout << "Producer: " << cart[index] << std::endl;
+        index = (index + 1) % 2;
         sem_post(sem_mutex);
         sem_post(sem_full);
+        sleep(1);
     }
     
 
